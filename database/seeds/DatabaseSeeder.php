94ff7bcfe3974f02f3189257c1c3c7123453c0a3
2006078 <2006078@itg.ac.id>
2022-12-09 00:47:05 +0700

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,17 +12,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(SliderTableSeeder::class);
-        $this->call(RoomTypesTableSeeder::class);
-        $this->call(ImagesTableSeeder::class);
+        // $this->call(SliderTableSeeder::class);
+        // $this->call(RoomTypesTableSeeder::class);
+        // $this->call(ImagesTableSeeder::class);
         $this->call(FacilitiesTableSeeder::class);
-        $this->call(FacilityRoomTypeTableSeeder::class);
-        $this->call(RoomsTableSeeder::class);
-        $this->call(EventsTableSeeder::class);
-        $this->call(FoodsTableSeeder::class);
-        $this->call(EventBookingsTableSeeder::class);
-        $this->call(RoomBookingsTableSeeder::class);
-        $this->call(ReviewsTableSeeder::class);
-        $this->call(PagesTableSeeder::class);
+        // $this->call(FacilityRoomTypeTableSeeder::class);
+        // $this->call(RoomsTableSeeder::class);
+        // $this->call(EventsTableSeeder::class);
+        // $this->call(FoodsTableSeeder::class);
+        // $this->call(EventBookingsTableSeeder::class);
+        // $this->call(RoomBookingsTableSeeder::class);
+        // $this->call(ReviewsTableSeeder::class);
+        // $this->call(PagesTableSeeder::class);
     }
 }
